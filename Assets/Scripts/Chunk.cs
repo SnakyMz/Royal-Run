867cfab289f3a20aaaf3150bbcb1b052fde9b269
Soban Syeed <Soban.syeed@outlook.com>
2025-05-10 08:28:22 +0530

    Add Apple Pickup Spawning functionality

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,28 +4,47 @@ using System.Collections.Generic;
 public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applePrefab;
     [SerializeField] float[] lanes = { -2.5f, 0f, 2.5f };
+
+    List<int> availableLanes = new List<int> { 0, 1, 2 };
 
     void Start()
     {
-        SpawnFence();
+        SpawnFences();
+        SpawnApple();
     }
 
-    void SpawnFence()
+    void SpawnFences()
     {
         int fencesToSpawn = Random.Range(0, lanes.Length);
-        List<int> availableLanes = new List<int> { 0, 1, 2 };
 
         for (int i = 0; i < fencesToSpawn; i++)
         {
             if (availableLanes.Count <= 0) break;
 
-            int randomIndex = Random.Range(0, availableLanes.Count);
-            int selectedLane = availableLanes[randomIndex];
-            availableLanes.RemoveAt(randomIndex);
+            int selectedLane = SelectLane();
 
             Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fencePrefab, spawnPosition, Quaternion.identity, transform);
         }
+    }
+
+    void SpawnApple()
+    {
+        if (availableLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, transform);
+    }
+
+    int SelectLane()
+    {
+        int randomIndex = Random.Range(0, availableLanes.Count);
+        int selectedLane = availableLanes[randomIndex];
+        availableLanes.RemoveAt(randomIndex);
+        return selectedLane;
     }
 }
