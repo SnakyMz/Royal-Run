6bfefc2c490c8833a0a05f809c23269f35d6b0f7
Soban Syeed <Soban.syeed@outlook.com>
2025-05-10 23:41:19 +0530

    Add Stumble Animation cooldown

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f;
 
     const string hitTrigger = "Hit";
+    float cooldownTimer = 0f;
 
     void OnCollisionEnter(Collision other)
     {
+        if (cooldownTimer < collisionCooldown) return;
+
         animator.SetTrigger(hitTrigger);
+        cooldownTimer = 0f;
     }
 }
