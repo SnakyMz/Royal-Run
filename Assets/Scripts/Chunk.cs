de38f2028d6a8dea3220c2908e39773819e9e0ea
Soban Syeed <Soban.syeed@outlook.com>
2025-05-10 08:37:04 +0530

    Add Coin Pickup spawning funcitonality

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -5,7 +5,9 @@ public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject fencePrefab;
     [SerializeField] GameObject applePrefab;
+    [SerializeField] GameObject coinPrefab;
     [SerializeField] float appleSpawnChance = 0.3f;
+    [SerializeField] float coinSpawnChance = 0.5f;
     [SerializeField] float[] lanes = { -2.5f, 0f, 2.5f };
 
     List<int> availableLanes = new List<int> { 0, 1, 2 };
@@ -14,6 +16,7 @@ public class Chunk : MonoBehaviour
     {
         SpawnFences();
         SpawnApple();
+        SpawnCoins();
     }
 
     void SpawnFences()
@@ -34,6 +37,16 @@ public class Chunk : MonoBehaviour
     void SpawnApple()
     {
         if (Random.value > appleSpawnChance || availableLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, transform);
+    }
+
+    void SpawnCoins()
+    {
+        if (Random.value > coinSpawnChance || availableLanes.Count <= 0) return;
 
         int selectedLane = SelectLane();
 
