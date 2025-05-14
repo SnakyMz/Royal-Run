a03f6f8960cec7d157945f12813aef90f57e33c2
Soban Syeed <Soban.syeed@outlook.com>
2025-05-14 09:32:16 +0530

    Add Rock VFX and Sound

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -3,19 +3,47 @@ using Unity.Cinemachine;
 
 public class Rock : MonoBehaviour
 {
+    [SerializeField] ParticleSystem collisionParticle;
     [SerializeField] float impulseModifier = 10f;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] float cooldown = 1f;
 
     CinemachineImpulseSource cinemachineImpulseSource;
+    float cooldownTimer = 0f;
 
     void Awake()
     {
         cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
+    {
+        if (cooldownTimer < cooldown) return;
+
+        FireImpulse();
+        CollisionFX(other);
+
+        cooldownTimer = 0f;
+    }
+
+    void FireImpulse()
     {
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float intensity = (1f / distance) * impulseModifier;
+        intensity = Mathf.Min(intensity, 1f);
         cinemachineImpulseSource.GenerateImpulse(intensity);
+    }
+
+    void CollisionFX(Collision other)
+    {
+        ContactPoint contact = other.contacts[0];
+        collisionParticle.transform.position = contact.point;
+        collisionParticle.Play();
+        audioSource.Play();
     }
 }
